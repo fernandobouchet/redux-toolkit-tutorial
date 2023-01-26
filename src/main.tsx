da45c2952c928e0210174b566a7e33be22f1f507
fernandobouchet <fernandobouchet@gmail.com>
2023-01-26 20:25:22 -0300

    Added 'simplecss' dependencies and add the import on 'main.tsx'

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import './index.css';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
+import 'simpledotcss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
